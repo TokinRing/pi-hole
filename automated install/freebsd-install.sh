7896762d1174a61c2645c20c555236302a498264
TokinRing <support@gnetsolutions.net>
2018-03-23 21:59:34 -0700

    fix freebsd detection

--- automated install/freebsd-install.sh
+++ automated install/freebsd-install.sh
@@ -404,7 +404,7 @@ resetRepo() {
 find_IPv4_information() {
   os_version=`uname`
   # Check if the host is FreeBSD
-  if [ os_version == "FreeBSD" ]; then
+  if os_version == "FreeBSD"; then
     # Named, local variables
     local route
     # Find IP used to route to outside world by checking the the route to Google's public DNS server
@@ -437,7 +437,7 @@ find_IPv4_information() {
 # Get available interfaces that are UP
 get_available_interfaces() {
   os_version=`uname`
-  if [ os_version == "FreeBSD" ]; then
+  if os_version == "FreeBSD"; then
     # There may be more than one so it's all stored in a variable
     availableInterfaces=$(ifconfig -l | tr ' ' '\n')
   else
